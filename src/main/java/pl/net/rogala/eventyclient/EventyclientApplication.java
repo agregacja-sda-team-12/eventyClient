60783215e66c3d4a8b603e7a927c8b96d87cf3cd
Marlena Rogala <marlenalukomska@wp.pl>
2019-03-17 11:22:21 +0100

    initial commit

---
+++ src/main/java/pl/net/rogala/eventyclient/EventyclientApplication.java
@@ -0,0 +1,13 @@
+package pl.net.rogala.eventyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventyclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventyclientApplication.class, args);
+    }
+
+}
